e67f2b5abeaf8bbb84872a9a00735e82a0d7a29f
zenin5479 <zenin5479@gmail.com>
2025-11-22 20:57:55 +0300

    Update 9.1 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -112,8 +112,6 @@ namespace WrongWriteCodePartTwo
          Console.WriteLine("--------------------------------------------------------------------------------");
          Console.WriteLine("Находение строки с наименьшей суммой элементов прямоугольного двумерного массива");
          Console.WriteLine("--------------------------------------------------------------------------------");
-
-
          Console.Write("Введите количество строк массива: ");
          int rows = Convert.ToInt32(Console.ReadLine());
          Console.Write("Введите количество столбцов массива: ");
@@ -155,7 +153,7 @@ namespace WrongWriteCodePartTwo
             }
          }
 
-  // Метод нахождения строки в массиве с наименьшей суммой элементов
+         // Метод нахождения строки в массиве с наименьшей суммой элементов
          void FindRowLowAmount()
          {
             int rowMinSum = 1;
@@ -182,13 +180,11 @@ namespace WrongWriteCodePartTwo
 
             Console.WriteLine("минимальная сумма строк = " + rowMinSum + ", в строке под номером: " + (minI + 1));
          }
+
          CompletionMassif();
          Console.WriteLine("Прямоугольный массив");
          OutputMassif();
-       
          Console.WriteLine("Строка с наименьшей суммой элементов");
-
-
          FindRowLowAmount();
 
          Console.WriteLine("-------------------------------");
