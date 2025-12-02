906e83c469a4d2f1c9e348b603846f4efa9ebc74
zenin5479 <zenin5479@gmail.com>
2025-12-02 09:30:21 +0300

    Update 26.7 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -251,7 +251,7 @@ namespace WrongWriteCodePartTwo
          int[,,] parade = new int[chanceone, chancetwo, chancethree];
 
          // Метод заполнения прямоугольного 3-х мерного массива
-         void CompletionArr()
+         void CompletionCollection()
          {
             Random casual = new Random();
             int forceone = parade.GetLength(0);
@@ -337,7 +337,7 @@ namespace WrongWriteCodePartTwo
             }
          }
 
-         CompletionArr();
+         CompletionCollection();
          PrintCollection();
 
          Console.WriteLine("---------------------------------------");
