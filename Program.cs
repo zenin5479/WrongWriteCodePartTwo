c8c1314d277c2b7f093e09a683df0e9b0dd53d19
zenin5479 <zenin5479@gmail.com>
2025-12-05 13:10:02 +0300

    Update 28.4 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -395,7 +395,6 @@ namespace WrongWriteCodePartTwo
 
             k++;
             Console.WriteLine();
-
          }
 
          Console.WriteLine("---------------------------------------------------------------------------------");
