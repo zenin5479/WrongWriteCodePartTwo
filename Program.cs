bcfeb5da4d3989f5e23a95b7f4a8557eaeb32770
zenin5479 <zenin5479@gmail.com>
2025-12-11 14:26:37 +0300

    Update 39.5 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -800,7 +800,7 @@ namespace WrongWriteCodePartTwo
             int j = 0;
             while (j < search.GetLength(1))
             {
-               if (Math.Abs(search[i, j]) < min && Math.Abs(search[i, j]) > 0)
+               if (Math.Abs(search[i, j]) < min)
                {
                   min = search[i, j];
                }
