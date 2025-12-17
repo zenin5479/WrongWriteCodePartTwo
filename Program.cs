364eb5b5a3dd738a4d26c7182e5a8afc3f140c99
zenin5479 <zenin5479@gmail.com>
2025-12-17 16:24:58 +0300

    Update 45.3 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -766,7 +766,7 @@ namespace WrongWriteCodePartTwo
                   int l = 0;
                   while (l < package.GetLength(1))
                   {
-                     Console.Write("{0} ", table[k, l]);
+                     Console.Write("{0} ", package[k, l]);
                      l++;
                   }
 
@@ -786,7 +786,7 @@ namespace WrongWriteCodePartTwo
                   int n = 0;
                   while (n < package.GetLength(0))
                   {
-                     Console.Write("{0} ", table[n, m]);
+                     Console.Write("{0} ", package[n, m]);
                      n++;
                   }
 
