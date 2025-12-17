46af4b2287349f869f9f1c64b2e752e9c11a7968
zenin5479 <zenin5479@gmail.com>
2025-12-17 15:15:46 +0300

    Update 42.9 Обновлен метод. Проведен рефакторинг

--- Program.cs
+++ Program.cs
@@ -805,21 +805,5 @@ namespace WrongWriteCodePartTwo
          return min;
       }
 
-      // Метод для вывода матрицы
-      static void PrintMatrix(int[,] matrix)
-      {
-         int rows = matrix.GetLength(0);
-         int cols = matrix.GetLength(1);
-
-         for (int i = 0; i < rows; i++)
-         {
-            for (int j = 0; j < cols; j++)
-            {
-               Console.Write("{0}\t", matrix[i, j]);
-            }
-
-            Console.WriteLine();
-         }
-      }
    }
 }
